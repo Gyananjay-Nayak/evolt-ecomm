d5b94f013d0f5298fc1bda6349c77cdc2b602769
Gyananjay Nayak <nayakgyananjay1999@gmail.com>
2024-06-05 02:27:44 +0530

    added error boundary

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import './App.css';
 import { Route, Routes, Navigate } from 'react-router-dom';
 import routes from './routes/allRoutes';
-// import ErrorBoundary from './components/ErrorBoundary';
+import ErrorBoundary from './components/ErrorBoundary';
 
 function App() {
   return (
     <>
-      {/* <ErrorBoundary> */}
-      <Routes>
-        {routes.map((eachComponent) => (
-          <Route
-            path={eachComponent.path}
-            element={eachComponent.component}
-            key={eachComponent.path}
-          />
-        ))}
-        <Route path="/" element={<Navigate to="products" replace />} />
-      </Routes>
-      {/* </ErrorBoundary> */}
+      <ErrorBoundary>
+        <Routes>
+          {routes.map((eachComponent) => (
+            <Route
+              path={eachComponent.path}
+              element={eachComponent.component}
+              key={eachComponent.path}
+            />
+          ))}
+          <Route path="/" element={<Navigate to="products" replace />} />
+        </Routes>
+      </ErrorBoundary>
     </>
   );
 }
